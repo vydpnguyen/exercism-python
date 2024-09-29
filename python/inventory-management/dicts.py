30f0843d5b0392355667aaf7c2a479ab0efdd12d
Vy Nguyen <vydpnguyen@gmail.com>
2024-09-29 14:03:25 -0700

    Add final code

--- python/inventory-management/dicts.py
+++ python/inventory-management/dicts.py
@@ -1,14 +1,21 @@
 """Functions to keep track and alter inventory."""
 
-
+def main():
+    print(create_inventory(["wood", "iron", "iron", "diamond", "diamond"]))
+    print(add_items({"coal": 1}, ["wood", "iron", "coal", "wood"]))
+    print(decrement_items({"coal":2, "wood":1, "diamond":2}, ["coal", "coal", "wood", "wood", "diamond"]))
+    
 def create_inventory(items):
     """Create a dict that tracks the amount (count) of each element on the `items` list.
 
     :param items: list - list of items to create an inventory from.
     :return: dict - the inventory dictionary.
     """
-
-    pass
+    inventory = {}
+    for item in items:
+        inventory[item] = items.count(item)
+    print(inventory.items())
+    return inventory
 
 
 def add_items(inventory, items):
@@ -18,8 +25,12 @@ def add_items(inventory, items):
     :param items: list - list of items to update the inventory with.
     :return: dict - the inventory updated with the new items.
     """
-
-    pass
+    for item in items:
+        if item in inventory:
+            inventory[item] += 1
+        else:
+            inventory[item] = 1
+    return inventory
 
 
 def decrement_items(inventory, items):
@@ -29,8 +40,10 @@ def decrement_items(inventory, items):
     :param items: list - list of items to decrement from the inventory.
     :return: dict - updated inventory with items decremented.
     """
-
-    pass
+    for item in items:
+        if item in inventory and inventory[item] != 0:
+            inventory[item] -= 1
+    return inventory
 
 
 def remove_item(inventory, item):
@@ -41,7 +54,9 @@ def remove_item(inventory, item):
     :return: dict - updated inventory with item removed. Current inventory if item does not match.
     """
 
-    pass
+    if item in inventory:
+        inventory.pop(item)
+    return inventory
 
 
 def list_inventory(inventory):
@@ -50,6 +65,12 @@ def list_inventory(inventory):
     :param inventory: dict - an inventory dictionary.
     :return: list of tuples - list of key, value pairs from the inventory dictionary.
     """
+    my_list = []
+    for item in inventory:
+        if inventory[item] != 0:
+            my_list.append((item, inventory[item]))
+    return  my_list
 
-    pass
+
+#main()
 
